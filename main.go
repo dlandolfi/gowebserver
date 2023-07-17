b89bf9756f425957f266d10ed53e684b42b5c240
dlandolfi <16345531+dlandolfi@users.noreply.github.com>
2023-07-16 20:19:24 -0400

    adding middleware that applies response headers

--- main.go
+++ main.go
@@ -15,16 +15,26 @@ func main() {
 	var rootDirPtr = flag.String("rootDir", "/root/gowebserver", "Use to change working directory")
 	flag.Parse()
 
-	mux := http.NewServeMux()
+	applyHeaders := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-type", "text/html; charset=utf-8")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+			w.Header().Set("Pragma", "no-cache")
+
+			next.ServeHTTP(w, r)
+		})
+	}
 
 	// handlers
+	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(*rootDirPtr+"/public")))
 	mux.HandleFunc("/test", test)
 	mux.Handle("/favicon.ico", http.NotFoundHandler())
 	mux.Handle("/testt", http.NotFoundHandler())
 
 	fmt.Println("Listening on port " + port + "...")
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, applyHeaders(mux)))
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
